12fe894c5de353d0b1ffaabc906b719ed1fd7fbe
bagriffin11 <48770963+bagriffin11@users.noreply.github.com>
2022-01-20 23:27:26 -0800

    go back

--- client/src/Global/Variables.js
+++
@@ -1,9 +0,0 @@
-import { createContext} from 'react';
-
-export const LoginContext = createContext({});
-
-export const UserId = createContext();
-
-export const UserName = createContext({});
-
-export const UserEmail = createContext({});
